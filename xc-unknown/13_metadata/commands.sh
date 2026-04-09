7aa6175521442e445c8394cec75060003a507ea6
David Studholme <d.j.studholme@exeter.ac.uk>
2026-04-09 11:37:33 +0000

    Gathered metadata

---
+++ xc-unknown/13_metadata/commands.sh
@@ -0,0 +1,9 @@
+### Install NCBI EDirect tool
+sh -c "$(curl -fsSL https://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh)"
+export PATH=${HOME}/edirect:${PATH}
+
+perl ./get_metadata_for_genomes.pl > genomes-metadata.tsv
+
+
+
+
